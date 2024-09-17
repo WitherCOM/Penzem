ff65257c97ebca135c2cd52e8eba0c86d9cea9c3
Németh Csaba Tibor <nemeth.csaba.tibor@gmail.com>
2024-09-17 20:31:45 +0200

    Reworked category

--- app/Filament/Resources/BudgetResource/RelationManagers/ChildBudgetsRelationManager.php
+++ app/Filament/Resources/BudgetResource/RelationManagers/ChildBudgetsRelationManager.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\BudgetResource\RelationManagers;
 
 use App\Models\Budget;
+use App\Models\Category;
+use CodeWithDennis\FilamentSelectTree\SelectTree;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -19,6 +22,34 @@ class ChildBudgetsRelationManager extends RelationManager
     {
         return $form
             ->schema([
+                SelectTree::make('categories')
+                    ->statePath('categories')
+                    ->live()
+                    ->relationship('categories','name','parent_category_id')
+                    ->afterStateUpdated(function (array $state, Set $set) {
+                        $categoryIds = collect([]);
+                        foreach($state as $category_id)
+                        {
+                            $categoryModel = Category::find($category_id);
+                            $validId = true;
+                            foreach ($state as $category_id2)
+                            {
+                                if ($categoryModel->getChildIds()->contains($category_id2))
+                                {
+                                    $validId = false;
+                                    break;
+                                }
+                            }
+                            if ($validId)
+                            {
+                                $categoryIds->push($category_id);
+                            }
+                        }
+                        $set('categories',$categoryIds->toArray());
+                    })
+                    ->enableBranchNode()
+                    ->searchable()
+                    ->required(),
                 Forms\Components\TextInput::make('description')
                     ->required()
                     ->maxLength(255),
@@ -32,7 +63,7 @@ class ChildBudgetsRelationManager extends RelationManager
             ->columns([
                 Tables\Columns\TextColumn::make('amount')
                     ->currency(fn (Budget $record) => $record->currency->name, true),
-                Tables\Columns\TextColumn::make('category.name'),
+                Tables\Columns\TextColumn::make('categories.name'),
                 Tables\Columns\TextColumn::make('description'),
             ])
             ->filters([
